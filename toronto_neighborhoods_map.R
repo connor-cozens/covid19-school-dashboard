17f69c47c01883d44dc30d4ce4053bb91f1462e4
Peter Taylor <peter@arbit.ca>
2020-11-24 18:09:26 -0500

    clean up

--- toronto_neighborhoods_map.R
+++ toronto_neighborhoods_map.R
@@ -27,11 +27,14 @@ idx <- which(risk_rank_neighborhood$neighborhood_name %in% centers$neighborhood_
 # put risk rank risk_rank_neighborhood neighborhood_name in same order as in centers
 idx <- sapply(centers$neighborhood_name, function(x) which(risk_rank_neighborhood$neighborhood_name == x)) %>% as.integer
 rr <- risk_rank_neighborhood[ idx, ]
+idx <- which(is.na(rr$of_100))
+rr[ idx, 'of_100' ] <- 0
 
 basemap <- leaflet(neighborhoods)
 basemap <- setView(basemap, lng = -79.41432, lat = 43.69453, zoom = 13)
-popup_html <- sprintf('<strong>%s</strong><br>Population: %s<br>7-Day Count: %s<br>One Infection Per: %s<br>Transmissible Cases before Isolation and Seroprevalence: %s<br>Population Density Per Square Kilometre: %s<br>Average Household Size: %s<br>Prevalence of low income based on the Low-income cut-offs, after tax (LICO-AT) (%%): %s<br>', 
+popup_html <- sprintf('<strong>%s</strong><br>Probability of contracting virus in crowd of 100: %s<br>Population: %s<br>7-Day Count: %s<br>One Infection Per: %s<br>Transmissible Cases before Isolation and Seroprevalence: %s<br>Population Density Per Square Kilometre: %s<br>Average Household Size: %s<br>Prevalence of low income based on the Low-income cut-offs, after tax (LICO-AT) (%%): %s<br>', 
 					  rr$neighborhood_name,
+					  rr$of_100,
 					  as.integer(rr$Population),
 					  as.integer(rr$`7-Day Count`),
 					  as.numeric(rr$`One Infection Per`),
@@ -44,7 +47,7 @@ basemap <- addPolygons(basemap,
 					   weight = 1, 
 					   fillColor = 'red', 
 					   smoothFactor = 0.2, 
-					   fillOpacity = risk_rank_neighborhood$of_100,
+					   fillOpacity = rr$of_100,
 					   popup = popup_html) %>%
 	addLabelOnlyMarkers(data = centers,
 						lng = ~x, lat = ~y, label = ~neighborhood_name,
